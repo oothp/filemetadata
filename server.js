7cebf284cd6500fa59b90ef1ccc9dc96b91a4c7e
Miglé <migle@sky.com>
2022-04-25 21:17:06 +0300

    up

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const multer = require("multer")
+require('dotenv').config()
+
+//Configuration for Multer
+const upload = multer({ dest: "public/files" })
+
+const app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/fileanalyse', upload.single("upfile"), (req, res) => {
+  let result = {
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  }
+  
+  res.send(result)
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
+});
